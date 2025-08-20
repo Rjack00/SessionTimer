cca507d3c5dedecd44d00defe5629609a8aa1088
Rjack00 <Rjackson77@comcast.net>
2025-08-20 14:16:08 -0600

    src/components updated
    TimerDisplay function added

---
+++ src/components/TimerDisplay.jsx
@@ -0,0 +1,15 @@
+export default function TimerDisplay({timerLabel, timeRemaining}) {
+    const minutes = Math.floor(timeRemaining / 60);
+    const seconds = timeRemaining % 60;
+
+    const formatTime = (num) => (num < 10 ? `0${num}` : num);
+
+    return (
+        <div id="timer-container">
+            <div id="timer-label">{timerLabel}</div>
+            <div id="time-left">
+                {`${formatTime(minutes)}:${formatTime(seconds)}`}
+            </div>
+        </div>
+    );
+}
